374bb5f39961d8becc860d18043866b5f50ac6b6
E-zn131 <eyadzanaty131@gmail.com>
2024-10-20 18:57:06 +0300

    Updating Databases

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -20,7 +20,6 @@ CREATE TABLE Customers(
 CREATE TABLE Orders(
     order_id INT PRIMARY KEY,
     customer_id INT,
-    FOREIGN KEY (customer_id) REFERENCES Customers (customer_id),
     order_date DATE
 );
 CREATE TABLE Order_Details(
