418d5a31969d8e5dd9985bc818dcc082bcc468b0
EyadZn <84345073+Eyad-Zanaty@users.noreply.github.com>
2024-10-20 19:02:56 +0300

    Update task_2.sql

--- task_2.sql
+++ task_2.sql
@@ -1,5 +1,4 @@
-USE alx_book_store;
-
+CREATE DATABASE IF NOT EXISTS alx_book_store; 
 CREATE TABLE Books(
     book_id INT PRIMARY KEY,
     title VARCHAR(130),
@@ -8,31 +7,27 @@ CREATE TABLE Books(
     price DOUBLE,
     publication_date DATE
 );
-
 CREATE TABLE Authors(
     author_id INT PRIMARY KEY,
     author_name VARCHAR(215)
 );
-
 CREATE TABLE Customers(
     customer_id INT PRIMARY KEY,
     customer_name VARCHAR(215),
     email VARCHAR(215),
     address TEXT
 );
-
 CREATE TABLE Orders(
     order_id INT PRIMARY KEY,
     customer_id INT,
-    FOREIGN KEY (customer_id) REFERENCES Customers (customer_id),
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
     order_date DATE
 );
-
 CREATE TABLE Order_Details(
     orderdetailid INT PRIMARY KEY,
     order_id INT,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     book_id INT,
-    quantity DOUBLE,
-    FOREIGN KEY (order_id) REFERENCES Orders (order_id),
-    FOREIGN KEY (book_id) REFERENCES Books (book_id)
-    );
+    FOREIGN KEY (book_id) REFERENCES Books(book_id),
+    quantity DOUBLE
+);
